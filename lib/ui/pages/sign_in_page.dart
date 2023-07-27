af04e5c8ed1574ba4fed321ab06b54331fd5593c
Dhanuwrdhn <dhanuwardhan09@gmail.com>
2023-07-27 11:31:27 +0700

    add Custom Widget Form

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
@@ -45,53 +46,15 @@ class SignInPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               //Email Input
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text('Email Address',
-                style: blackTextStyle.copyWith(
-                  fontWeight: medium
-                ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    contentPadding: const EdgeInsets.all(12)
-                  ),
-                ),
-              ],
-              ),
+            const CustomFormField(title: 'Email Address',
+            ),
               const SizedBox(
                 height: 16,
               ),
               //Password Input
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text('Password',
-                style: blackTextStyle.copyWith(
-                  fontWeight: medium
-                ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    contentPadding: const EdgeInsets.all(12)
-                  ),
-                ),
-              ],
-              ),
+          const CustomFormField(title: 'Password',
+          obscureText: true,
+            ),
               const SizedBox(
                 height: 8,
               ),
